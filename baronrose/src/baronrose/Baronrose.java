553dff5b707452ee15e283da7c542ca1682db359
BARON-ROSE <rosebaron1234@gmail.com>
2020-10-24 05:17:20 -0700

    first commit try

---
+++ baronrose/src/baronrose/Baronrose.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package baronrose;
+
+/**
+ *
+ * @author User
+ */
+public class Baronrose {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String p = "string";
+        String o = "bb";
+        boolean f = true;
+        boolean t = false;
+        if (p.contains(o)){
+        
+        System.out.println(f);
+        }else
+         System.out.println(t);   
+            
+    }
+    
+}

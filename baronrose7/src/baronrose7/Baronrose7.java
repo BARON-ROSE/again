5943ce570b25c33885b0d4305d5f586e20b90b47
BARON-ROSE <rosebaron1234@gmail.com>
2020-10-31 04:31:24 -0700

    wala lg

---
+++ baronrose7/src/baronrose7/Baronrose7.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package baronrose7;
+
+/**
+ *
+ * @author User
+ */
+public class Baronrose7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       int [][] array = new int [10][10];
+       
+       for(int i = 0; i < 10; i++){
+        int ii = 0;
+            while (ii < 10){
+           
+            System.out.printf("%2d", array[i][ii]);
+            ii++;
+            
+            }
+            System.out.println(); 
+    
+        }
+    }
+    
+}
+
+
+
+
+       
+
